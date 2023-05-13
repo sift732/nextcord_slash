fa7d8b2b921a0721339f283ebe47baed89af14d7
darkguide <83960086+sift732@users.noreply.github.com>
2023-05-13 17:04:45 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nextcord_slash',
     version='0.1',
     description='A package for creating and handling Discord slash commands with the nextcord library',
-    author='Your Name',
-    author_email='your.email@example.com',
-    packages=['nextcord_slash'],
+    author='sift732',
+    author_email='kanariaking30@gmail.com',
+    packages=find_packages(),
     install_requires=[
         'nextcord',
         'discord-py-slash-command'
